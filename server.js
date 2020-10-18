0231d830deeb179509728ccc7f14fe9cef2c7ce3
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-10-18 21:42:54 +0200

    setup of the project finished

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ app.set('view engine', 'ejs')
 
 // if you go to /articles then render what's in articleRouter
 // if we add more routes in articles then they're also reachable
+// this is basically the way to reach the rest of the site
 app.use('/articles', articleRouter)
 
-
-
-
+// this is the default landing page of the site located at '/'
 app.get('/', (req, res) => {
-    res.render('index')
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+
+    const articles = [{
+        title: 'This is a title',
+        createdAt: today.toDateString(),
+        description: 'test description'
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
