69a390ad60c81eb2d0446b881eea03e7faa0c906
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-10-25 22:38:20 +0100

    how does this even work

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,40 @@
 const express = require('express')
+const Article = require('../models/article')
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.send('In articles')
+// in our serverJS this equates to localhost:5000/articles though !
+// router.get('/', (req,res) => {
+//     res.send('In articles')
+// })
+
+// and this to localhost:5000/articles/test
+router.get('/new', (req,res) => {
+    res.render('articles/new', { article: new Article() }) 
 })
 
-router.get('/test', (req,res) => {
-    res.send('In test')
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) {
+        res.redirect('/')
+    }
+    res.render('articles/show', { article: article })
+    // https://youtu.be/1NrHkjlWVhM?t=2097
+})
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        console.log(e);
+        res.render('articles/new', { article: article})
+    }
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true, useUnifiedTopology: true })
+    //https://stackoverflow.com/questions/59455725/install-mongodb-on-manjaro#:~:text=Simply%20clone%20the%20repo%20in,command%20to%20make%20the%20package.
+
+
 app.set('view engine', 'ejs')
+
+
+// this next line allows us to access any element of the front end in order to get data from it (like a form or anything else)
+app.use(express.urlencoded({ extended: false }))
+
+
+// this is the default landing page of the site located at '/'
+// when you go to '/' then this is rendered
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', { articles: articles }) //when going to '/' we'll render the html of articles/index.ejs
+})
+
+// app.get('/new', (req, res) => {
+//     res.render('articles/new') //when going to '/' we'll render the html of articles/index.ejs
+// })
+
 
 // if you go to /articles then render what's in articleRouter
 // if we add more routes in articles then they're also reachable
 // this is basically the way to reach the rest of the site
+// Currently, articleRouter equals /routes/articles. 
+// in articlesJS this is the default route '/', in here it equals to /articles since that's what we're saying in this next piece of code.
 app.use('/articles', articleRouter)
-
-// this is the default landing page of the site located at '/'
-app.get('/', (req, res) => {
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);
-
-    const articles = [{
-        title: 'This is a title',
-        createdAt: today.toDateString(),
-        description: 'test description'
-    }]
-    res.render('index', { articles: articles })
-})
 
 app.listen(5000)
